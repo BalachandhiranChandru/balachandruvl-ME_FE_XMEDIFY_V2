b6be6b69ea76d3d53f2d50804f44bb86f25a23a7
BalachandhiranChandru <119665136+BalachandhiranChandru@users.noreply.github.com>
2025-11-19 16:23:02 +0530

    Update AutohideSnackbar.jsx

--- src/components/AutohideSnackbar/AutohideSnackbar.jsx
+++ src/components/AutohideSnackbar/AutohideSnackbar.jsx
@@ -2,15 +2,12 @@ import { Alert } from '@mui/material';
 import Snackbar from '@mui/material/Snackbar';
 
 export default function AutohideSnackbar({ open, setOpen, message }) {
-
     const handleClose = (event, reason) => {
         if (reason === 'clickaway') {
             return;
         }
-
         setOpen(false);
     };
-
     return (
         <Snackbar
             open={open}
@@ -18,7 +15,7 @@ export default function AutohideSnackbar({ open, setOpen, message }) {
             onClose={handleClose}
             anchorOrigin={{ vertical: 'bottom', horizontal: 'center' }}
         >
-            <Alert onClose={handleClose} icon={false} sx={{bgcolor:'primary.green', color:'#fff'}}>
+            <Alert onClose={handleClose} icon={false} sx={{ bgcolor: 'primary.green', color: '#fff' }}>
                 {message}
             </Alert>
         </Snackbar>
