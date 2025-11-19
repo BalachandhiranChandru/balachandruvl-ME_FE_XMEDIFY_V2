ab9be734a3b1e2c1531420ac009f2cd5fad530df
BalachandhiranChandru <119665136+BalachandhiranChandru@users.noreply.github.com>
2025-11-19 16:36:18 +0530

    Update OurFamilies.jsx

--- src/components/Sections/OurFamilies/OurFamilies.jsx
+++ src/components/Sections/OurFamilies/OurFamilies.jsx
@@ -1,12 +1,11 @@
 import { Box, Container, Grid, Typography } from "@mui/material";
-import banner from '../../../assets/our-families-banner.jpg'
+import banner from '../../../assets/our-families-banner.png'
 
 export default function OurFamilies() {
     return (
         <Box pt={3} sx={{ background: 'linear-gradient(#E7F0FF, #E8F1FF)' }}>
             <Container>
                 <Grid container alignItems='center' spacing={{xs:2,md:10}}>
-
                     <Grid item xs={12} md={6}>
                         <Typography
                             fontWeight={600}
@@ -21,7 +20,6 @@ export default function OurFamilies() {
                             We will work with you to develop individualised care plans, including management of chronic diseases. If we cannot assist, we can provide referrals or advice about the type of practitioner you require. We treat all enquiries sensitively and in the strictest confidence.
                         </Typography>
                     </Grid>
-
                     <Grid item xs={12} md={6}>
                         <Box
                             component='img'
@@ -30,7 +28,6 @@ export default function OurFamilies() {
                             height='auto'
                         />
                     </Grid>
-
                 </Grid>
             </Container>
         </Box>
