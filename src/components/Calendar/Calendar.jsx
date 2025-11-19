2de7abc82dee362408e100a4c16c6f9b0d07742c
BalachandhiranChandru <119665136+BalachandhiranChandru@users.noreply.github.com>
2025-11-19 16:24:47 +0530

    Update Calendar.jsx

--- src/components/Calendar/Calendar.jsx
+++ src/components/Calendar/Calendar.jsx
@@ -1,17 +1,15 @@
 import DaySelector from "./DaySelector/DaySelector";
 import { Box } from "@mui/material";
-import { useState } from "react";
 import { startOfDay } from "date-fns";
 import TimeSlotPicker from "./TimeSlotPicker/TimeSlotPicker";
+import { useState } from "react";
 
 export default function Calendar({ availableSlots, details, handleBooking }) {
   const [selectedDate, setSelectedDate] = useState(startOfDay(new Date()));
-
   const totalSlots =
     availableSlots.morning.length +
     availableSlots.afternoon.length +
     availableSlots.evening.length;
-
   return (
     <Box>
       <DaySelector
